0ef5c1f110259b8460255ed97864809baeecd28e
c2pig <c2pig@c2pigs-iMac.local>
2021-09-09 22:32:47 +0800

    init cdk8s project

---
+++ main.test.ts
@@ -0,0 +1,11 @@
+import { HelloKube } from './main';
+import {Testing} from 'cdk8s';
+
+describe('Placeholder', () => {
+  test('Empty', () => {
+    const app = Testing.app();
+    const chart = new HelloKube(app, 'test-chart');
+    const results = Testing.synth(chart)
+    expect(results).toMatchSnapshot();
+  });
+});
